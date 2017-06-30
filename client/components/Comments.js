4df6d7a65c1a47b4b66113825ca5a78973d2ec13
Jordan Bautista <jordanbau@gmail.com>
2017-06-30 09:11:46 -0700

    working remove button

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,7 +7,7 @@ const Comments = React.createClass({
         <p>
             <strong>{comment.user}</strong>
             {comment.text}
-            <button className="remove-comment">&times;</button>
+            <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
 
         </p>
       </div>
@@ -20,8 +20,10 @@ const Comments = React.createClass({
     const comment = this.refs.comment.value;
 
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
     
   },
+  
   render() {
     return(
       <div className="comments">
